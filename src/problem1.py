67ac9cde1142521e514e43db31a2e193568f6732
ReidCameron <reidcj@rose-hulman.edu>
2018-05-24 13:20:34 -0400

    Finished problem 1

--- src/problem1.py
+++ src/problem1.py
@@ -2,9 +2,9 @@
 Final exam, problem 1.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and Cameron Reid.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem1a()
-    # run_test_problem1b()
+    run_test_problem1a()
+    run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -90,9 +90,12 @@ def problem1a(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+
+    total = numbers[0] + numbers[len(numbers) // 2] + numbers[len(numbers) - 1]
+    return total
 
 
 def run_test_problem1b():
@@ -166,10 +169,14 @@ def problem1b(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
+    total = 0
+    for k in range(len(numbers) // 3):
+        total = total + numbers[k + (len(numbers) // 3)]
+    return total
 
 ###############################################################################
 # Our tests use the following to print error messages in red.
